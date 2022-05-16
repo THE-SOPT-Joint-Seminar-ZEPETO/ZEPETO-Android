acddc7458b0301f44392b6d0829f184a2474cbf3
Choi Woo hyeong <gua05093@gmail.com>
2022-05-17 02:45:39 +0900

    [ADD] add Activity and FeedFragment

---
+++ app/src/main/java/org/sopt/zepeto/ui/EditImageActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.zepeto.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.zepeto.R
+
+class EditImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_image)
+    }
+}
--- app/src/main/java/org/sopt/zepeto/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/zepeto/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.sopt.zepeto
+package org.sopt.zepeto.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.zepeto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
