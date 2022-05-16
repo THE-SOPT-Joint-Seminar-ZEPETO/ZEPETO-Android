6c5ddb50231bb1f25acc692baa3d0f89f22222f7
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-17 04:21:08 +0900

    [UI] EditContentActivity 기초 뷰 작업 (폰트 적용 안됨)

---
+++ app/src/main/java/org/sopt/zepeto/EditContentActivity.kt
@@ -0,0 +1,15 @@
+package org.sopt.zepeto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.zepeto.databinding.ActivityEditContentBinding
+
+class EditContentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditContentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditContentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
