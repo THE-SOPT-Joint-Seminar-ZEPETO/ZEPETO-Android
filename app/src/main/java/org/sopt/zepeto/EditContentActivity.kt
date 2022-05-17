6881564e7556582b283f34ded315c4b0895eb63c
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-05-17 16:47:27 +0900

    [UI] EditContentActivity focus 여부에 따른 뷰 처리(키보드, 회색 background)

--- app/src/main/java/org/sopt/zepeto/EditContentActivity.kt
+++ app/src/main/java/org/sopt/zepeto/EditContentActivity.kt
@@ -1,6 +1,11 @@
 package org.sopt.zepeto
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.zepeto.databinding.ActivityEditContentBinding
 
@@ -11,5 +16,31 @@ class EditContentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditContentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setEditTextFocusListener()
+    }
+
+    private fun setEditTextFocusListener() {
+        binding.etContents.setOnFocusChangeListener { _, hasFocus ->
+            binding.isEditTextFocus = hasFocus
+        }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        val v = currentFocus
+        if (v != null && event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_MOVE && v is EditText && !v.javaClass
+            .name.startsWith("android.webkit.")
+        ) {
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
