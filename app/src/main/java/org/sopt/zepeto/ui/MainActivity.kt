e5f0b64b150bca8dfc8c2cec560780f1fa6e6e7e
Choi Woo hyeong <gua05093@gmail.com>
2022-05-18 15:39:33 +0900

    [FEAT] 갤러리에서 사진을 받아와 uri값을 EditImageActivity로 넘겨주는 코드 구현

--- app/src/main/java/org/sopt/zepeto/ui/MainActivity.kt
+++ app/src/main/java/org/sopt/zepeto/ui/MainActivity.kt
@@ -1,12 +1,42 @@
 package org.sopt.zepeto.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import org.sopt.zepeto.R
+import org.sopt.zepeto.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var imageUri : Uri = Uri.EMPTY
+    //
+       private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                imageUri = uri
+                val intent = Intent(this, EditImageActivity::class.java)
+                intent.putExtra("imageUri", imageUri)
+                startActivity(intent)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBtnClickListener()
+
     }
+
+    private fun initBtnClickListener() {
+
+        binding.button.setOnClickListener {
+            resultLauncher.launch("image/*")
+
+        }
+    }
+
 }
