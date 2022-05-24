b63581c310babaedfda05c002b1c0256e64d651c
Choi Woo hyeong <gua05093@gmail.com>
2022-05-25 01:32:36 +0900

    [FEAT] ZepetoService 에 getFeed()함수 추가

--- app/src/main/java/org/sopt/zepeto/data/service/ZepetoService.kt
+++ app/src/main/java/org/sopt/zepeto/data/service/ZepetoService.kt
@@ -1,6 +1,7 @@
 package org.sopt.zepeto.data.service
 
 import org.sopt.zepeto.data.response.BaseResponse
+import org.sopt.zepeto.data.response.ResponseFeed
 import org.sopt.zepeto.data.response.ResponseImages
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,4 +9,7 @@ import retrofit2.http.GET
 interface ZepetoService {
     @GET("images")
     fun getImages(): Call<BaseResponse<List<ResponseImages>>>
+
+    @GET("feed")
+    fun getFeed() : Call<BaseResponse<ResponseFeed>>
 }
